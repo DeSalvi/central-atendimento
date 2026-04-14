356bd97b5eda0587907db6ea74ed1f13a4cb7dcb
Pedro Henrique de Salvi <pedrosalvi47@gmail.com>
2026-04-14 01:10:26 -0300

    feat: implementa vetor dinamico com redimensionamento e suporte a Processo

--- src/main/java/br/com/central/estrutura/VetorDinamico.java
+++ src/main/java/br/com/central/estrutura/VetorDinamico.java
@@ -1,1 +1,37 @@
+package main.java.br.com.central.estrutura;
 
+import main.java.br.com.central.model.Processo;
+
+public class VetorDinamico {
+    private Processo[] v;
+    private int capacidade;
+    private int ocupacao;
+
+    public VetorDinamico(int capacidade_inicial) {
+        v = new Processo[capacidade_inicial];
+        capacidade = capacidade_inicial;
+        ocupacao = 0;
+    }
+
+    public boolean estaCheio() {
+        if (capacidade == ocupacao)
+            return true;
+        else
+            return false;
+    }
+
+    private void redimensiona(int novaCapacidade) {
+        Processo[] temp = new Processo[novaCapacidade];
+        for (int i = 0; i < ocupacao; i++) {
+            temp[i] = v[i];
+        }
+        v = temp;
+        capacidade = novaCapacidade;
+    }
+
+    public void add(Processo p) {
+        if (estaCheio())
+            redimensiona(capacidade * 2);
+        v[ocupacao++] = p;
+    }
+}
